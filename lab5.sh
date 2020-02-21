23d53ea2d6fc9f1b1b36da6600e3c93a0c9d5fd4
acow777 <acow777@gmail.com>
2020-02-20 17:17:28 -0700

    added script

---
+++ lab5.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+pip3 install opencv-contrib-python==4.1.0.25 opencv-python=4.1.0.25
+sudo apt install libhdf5-dev libatlas-base-dev libjasper-dev libqtgui4 libqt4-test libatomic1-arm64-cross cmake
+wget https://raw.githubusercontent.com/opencv/opencv/master/data/haarcascades/haarcascade_frontalface_default.xml
+wget https://raw.githubusercontent.com/opencv/opencv/master/data/haarcascades/haarcascade_eye.xml
+wget https://raw.githubusercontent.com/opencv/opencv/master/data/haarcascades/haarcascade_smile.xml
